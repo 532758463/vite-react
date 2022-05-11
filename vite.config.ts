d394387afd015f8d6ffc300e7c715dfd88db27a4
xiaoyang <532758463@qq.com>
2022-05-11 22:59:23 +0800

    chore: 配置调整

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
+import legacy from "@vitejs/plugin-legacy"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
+  plugins: [
+    react(),
+    chunkSplitPlugin({
+      // 抽取react、react-dom
+      customSplitting: {
+        "react-vendor": ["react", "react-dom"]
+      }
+    }),
+    legacy({
+      // 设置目标浏览器，browserslist 配置语法
+      targets: ["ie >= 11"]
+    })],
+  server: {
     port: 8080
   }
 })
