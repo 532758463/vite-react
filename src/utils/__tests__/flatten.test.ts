a2bdc602b16a212d7622db5ca1efa6d1f8f45033
xiaoyang <532758463@qq.com>
2022-06-04 17:01:50 +0800

    feat:任意层级数组扁平化

--- src/utils/__tests__/flatten.test.ts
+++ src/utils/__tests__/flatten.test.ts
@@ -1,12 +1,46 @@
-import { flatten } from '../flatten';
+import { flatten, flatten2, flatten3 } from '../flatten';
 
 describe('数组扁平化', () => {
   it('空数组', () => {
     const res: any[] = [];
     expect(flatten(res)).toEqual([]);
   });
-  it('二层数组', () => {
+  it('一级嵌套数组', () => {
     const res: any[] = [1, 2, [4, 5], 8];
     expect(flatten(res)).toEqual([1, 2, 4, 5, 8]);
+  });
+  it('二级嵌套数组', () => {
+    const res: any[] = [1, 2, [4, 5, [6, 7]], 8];
+    expect(flatten(res)).toEqual([1, 2, 4, 5, [6, 7], 8]);
+  });
+});
+
+describe('数组扁平化concat', () => {
+  it('空数组', () => {
+    const res: any[] = [];
+    expect(flatten2(res)).toEqual([]);
+  });
+  it('一级嵌套数组', () => {
+    const res: any[] = [1, 2, [4, 5], 8];
+    expect(flatten2(res)).toEqual([1, 2, 4, 5, 8]);
+  });
+  it('二级嵌套数组', () => {
+    const res: any[] = [1, 2, [4, 5, [6, 7]], 8];
+    expect(flatten2(res)).toEqual([1, 2, 4, 5, [6, 7], 8]);
+  });
+});
+
+describe('任意层级数组扁平化', () => {
+  it('空数组', () => {
+    const res: any[] = [];
+    expect(flatten3(res)).toEqual([]);
+  });
+  it('一级嵌套数组', () => {
+    const res: any[] = [1, 2, [4, 5], 8];
+    expect(flatten3(res)).toEqual([1, 2, 4, 5, 8]);
+  });
+  it('二级嵌套数组', () => {
+    const res: any[] = [1, 2, [4, 5, [6, 7]], 8];
+    expect(flatten3(res)).toEqual([1, 2, 4, 5, 6, 7, 8]);
   });
 });
--- src/utils/flatten.ts
+++ src/utils/flatten.ts
@@ -1,5 +1,5 @@
 /**
- * 数组扁平化
+ * 数组扁平化(一层)
  */
 export function flatten(arr: any[]): any[] {
   const res: any[] = [];
@@ -13,4 +13,36 @@ export function flatten(arr: any[]): any[] {
     });
   }
   return res;
+}
+
+/**
+ * 数组扁平化，使用concat
+ */
+
+export function flatten2(arr: any[]): any[] {
+  let res: any[] = [];
+  if (Array.isArray(arr)) {
+    arr.forEach((item) => {
+      res = res.concat(item);
+    });
+  }
+  return res;
+}
+
+/**
+ * 任意层级数组扁平化
+ * @param arr 输入的数组
+ * @param num 扁平化层级
+ */
+export function flatten3(arr: any[], num: number = Infinity): any[] {
+  if (Array.isArray(arr)) {
+    return num > 0
+      ? arr.reduce(
+          (prev: unknown[], cur: unknown[]) =>
+            prev.concat(Array.isArray(cur) ? flatten3(cur, num - 1) : cur),
+          []
+        )
+      : arr.slice();
+  }
+  return arr;
 }
