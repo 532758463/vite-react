57acc47c9a618a26ad6a7af23d4b5bd0f92ef5cb
xiaoyang <532758463@qq.com>
2022-06-04 17:11:38 +0800

    style:输出框样式优化

--- src/pages/console/index.tsx
+++ src/pages/console/index.tsx
@@ -9,7 +9,11 @@ export const ConsoleFn: React.FC = () => {
   const arr = [1, 2, [3, 5], 5];
   const res = flatten(arr);
 
-  return <div>{res}</div>;
+  return (
+    <div className="flex items-center justify-center m-5 border-2 border-blue-300 h-96">
+      {JSON.stringify(res)}
+    </div>
+  );
 };
 
 export default ConsoleFn;
