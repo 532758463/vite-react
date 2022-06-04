492dc82f914ca69582c9faa8a8c30701c9751e59
xiaoyang <532758463@qq.com>
2022-06-04 11:22:37 +0800

    chore:alias调整

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { chunkSplitPlugin } from "vite-plugin-chunk-split";
-import {
-  createStyleImportPlugin,
-  AntdResolve,
-} from 'vite-plugin-style-import'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
+import { createStyleImportPlugin, AntdResolve } from 'vite-plugin-style-import';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
+  resolve: {
+    alias: {
       '@src': resolve(__dirname, './src'),
+      '@utils': resolve(__dirname, './src/utils'),
+      '@pages': resolve(__dirname, './src/pages'),
+      '@components': resolve(__dirname, './src/components'),
+      '@hooks': resolve(__dirname, './src/hooks')
     }
   },
   css: {
     preprocessorOptions: {
       less: {
-        javascriptEnabled: true,
-      },
-    },
+        javascriptEnabled: true
+      }
+    }
   },
   plugins: [
     react(),
     chunkSplitPlugin({
       // 抽取react、react-dom
       customSplitting: {
-        "react-vendor": ["react", "react-dom"]
+        'react-vendor': ['react', 'react-dom']
       }
     }),
     createStyleImportPlugin({
-      resolves: [
-        AntdResolve(),
-      ],
+      resolves: [AntdResolve()],
       libs: [
         // If you don’t have the resolve you need, you can write it directly in the lib, or you can provide us with PR
         {
@@ -40,12 +39,12 @@ export default defineConfig({
           esModule: true,
           resolveStyle: (name) => {
             return `antd/es/${name}/style/index`;
-          },
-        },
-      ],
-    }),
+          }
+        }
+      ]
+    })
   ],
   server: {
     port: 8080
   }
-})
+});
